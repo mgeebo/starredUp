c280d2ea70e2d14b2a3c3955bbd61e9212f2a6ae
hmgriffiniv <hmgriffiniv@gmail.com>
2017-07-24 18:59:11 -0400

    Made UserController

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,8 +8,42 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\ORM\Mapping\Annotation;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\Container;
+use Swagger\Annotations as SWG;
 
-class UserController
+/**
+ * @SWG\Path(
+ *   path="/users"
+ * )
+ */
+
+class UserController extends ApiController
 {
+    protected $em;
+
+    public function __construct() {
+    }
+
+    /**
+     * @SWG\Get(
+     *     path="/users/{user_id}",
+     *     @SWG\Response(response="200", description="Success")
+     * )
+     *
+     * @Route("/users/{id}")
+     * @Method({"GET"})
+     */
+    public function getUser($id)
+    {
+
+        return new Response("User");
+    }
 
 }
