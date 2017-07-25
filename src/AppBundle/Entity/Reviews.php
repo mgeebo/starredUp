00b92beedb4d33458d934b7cee1288d374cf9395
Hankey <Daniel.Hankey@fmglobal.com>
2017-07-24 21:54:52 -0400

    Updated Review Class

--- src/AppBundle/Entity/Reviews.php
+++ src/AppBundle/Entity/Reviews.php
@@ -1,5 +1,7 @@
 <?php
 
+//<editor-fold desc="Description">...//</editor-fold>
+
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -12,24 +14,212 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Reviews
 {
+    //<editor-fold desc="Variables">
+
     /**
      * @var int
      *
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(name="review_id", type="integer")
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
-
+    private $reviewId;
 
     /**
-     * Get id
+     * @var int
      *
+     * @ORM\Column(name="product_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $productId;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="user_id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $userId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var double
+     *
+     * @ORM\Column(name="rating", type="float")
+     */
+    private $rating;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="create_date", type="DateTime")
+     */
+    private $createDate;
+
+    /**
+     * @var DateTime
+     *
+     * @ORM\Column(name="update_date", type="DateTime")
+     */
+    private $updateDate;
+
+    //</editor-fold>
+
+    //<editor-fold desc="Getters & Setters">
+
+    /**
      * @return int
      */
-    public function getId()
+    public function getReviewId()
     {
-        return $this->id;
+        return $this->reviewId;
     }
+
+    /**
+     * @param int $reviewId
+     */
+    public function setReviewId($reviewId)
+    {
+        $this->reviewId = $reviewId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProductId()
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @param int $productId
+     */
+    public function setProductId($productId)
+    {
+        $this->productId = $productId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return float
+     */
+    public function getRating()
+    {
+        return $this->rating;
+    }
+
+    /**
+     * @param float $rating
+     */
+    public function setRating($rating)
+    {
+        $this->rating = $rating;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * @param bool $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreateDate()
+    {
+        return $this->createDate;
+    }
+
+    /**
+     * @param DateTime $createDate
+     */
+    public function setCreateDate($createDate)
+    {
+        $this->createDate = $createDate;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdateDate()
+    {
+        return $this->updateDate;
+    }
+
+    /**
+     * @param DateTime $updateDate
+     */
+    public function setUpdateDate($updateDate)
+    {
+        $this->updateDate = $updateDate;
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Methods">
+
+    /*
+     * Will calculate the Rating based on an id or a number
+     */
+    public static function CalculateRating()
+    {}
+
+    //</editor-fold>
 }
 
